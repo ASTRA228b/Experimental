31429ff855694112cd7c3435c61b46a1b5696faa
M1 Interactive <36132911+m1llipede@users.noreply.github.com>
2026-06-09 02:25:44 -0500

    y

---
+++ Experimental-UIs/Core/IntroManager/IntroPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace Experimental.Core.IntroManager;
+
+public class IntroPlayer : MonoBehaviour
+{
+    private VideoPlayer? ExperimentalPlayer;
+    private RenderTexture? ExperimentalRender;
+    private bool Playin;
+
+    private void Start()
+    {
+
+    }
+    
+}
