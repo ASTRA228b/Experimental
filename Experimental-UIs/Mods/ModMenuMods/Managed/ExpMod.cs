ffa50490f087cc567046897d030d3404c4e15dc1
M1 Interactive <36132911+m1llipede@users.noreply.github.com>
2026-06-17 20:15:08 -0500

    yesseer

---
+++ Experimental-UIs/Mods/ModMenuMods/Managed/ExpMod.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Experimental.Mods.ModMenuMods.Managed;
+
+public abstract class ExpMod
+{
+    public string Name { get; }
+    public bool Enabled { get; private set; }
+
+    protected ExpMod(string name)
+    {
+        Name = name;
+    }
+
+    public virtual void Enable()
+    {
+        Enabled = true;
+    }
+
+    public virtual void Disable()
+    {
+        Enabled = false;
+    }
+
+    public virtual void Update() { }
+
+    public virtual void FixedUpdate() { }
+}
---
+++ Experimental-UIs/Mods/ModMenuMods/Managed/ModsManager.cs
@@ -0,0 +1,40 @@
+﻿namespace Experimental.Mods.ModMenuMods.Managed;
+
+public static class ModsManager
+{
+    private static readonly Dictionary<string, ExpMod> Mods = new();
+
+    public static void Register(string name, ExpMod mod)
+    {
+        Mods[name] = mod;
+    }
+
+    public static void Toggle(string name)
+    {
+        if (!Mods.TryGetValue(name, out var mod))
+            return;
+
+        if (mod.Enabled)
+            mod.Disable();
+        else
+            mod.Enable();
+    }
+
+    public static void Update()
+    {
+        foreach (var mod in Mods.Values)
+        {
+            if (mod.Enabled)
+                mod.Update();
+        }
+    }
+
+    public static void FixedUpdate()
+    {
+        foreach (var mod in Mods.Values)
+        {
+            if (mod.Enabled)
+                mod.FixedUpdate();
+        }
+    }
+}
